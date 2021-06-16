117e9edb0f45662e2e0d28541c3aa6470bc52182
tselikin <tselikin@gmail.com>
2021-06-16 10:01:44 +0300

    added some subscription tests

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -8,23 +8,35 @@ use Tests\TestCase;
 
 class ApiTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
     public function test_userCanSubscribeToSection() {
+        $response = $this->post(route('subscribe'), [
+            'email' => 'testmail@rambler.ru',
+            'section' => '1'
+        ]);
 
-                $response = $this->post(route('subscribe'), [
-                    'email' => 'testmail@rambler.ru',
-                    'section' => '1'
-                ]);
+        $response
+            ->assertStatus(201)
+            ->assertJson([
+                'created' => true,
+            ]);;
+    }
 
-                $response
-                    ->assertStatus(201)
-                    ->assertJson([
-                        'created' => true,
-                    ]);;
+    public function test_userCantSubscribeToSectionWithoutSectionID() {
+        $response = $this->post(route('subscribe'), [
+            'email' => 'testmail2@rambler.ru',
+            // 'section' => '1'
+        ]);
+
+        $response->assertStatus(302);
+    }
+
+    public function test_userCantSubscribeToNotExistingSection() {
+        $response = $this->post(route('subscribe'), [
+            'email' => 'testmail3@rambler.ru',
+            'section' => '100000'
+        ]);
+
+        $response->assertStatus(302);
     }
 
 }
